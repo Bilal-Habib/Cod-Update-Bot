7c313f89fb65f35b6450eff5ed0e9016d80ec6e4
55638034 <bilal.habib67@gmail.com>
2020-06-23 21:00:37 +0100

    installed more packages

--- scrape.js
+++ scrape.js
@@ -4,10 +4,11 @@ const cheerio = require('cheerio');
 request('https://support.activision.com/uk/en/modern-warfare/articles/latest-updates-for-call-of-duty-modern-warfare',
 (error, response, html) => {
     if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
+        console.log(html);
+        // const $ = cheerio.load(html);
 
-        const siteheading = $('.site-heading');
+        // const siteheading = $('.site-heading');
 
-        console.log(sitehhead)
+        // console.log(siteheading);
     }
 });
